583d3eaaeb86e676840fad5c85e58714687faaf7
nbr <nikolaus.brunner@protonmail.ch>
2025-04-21 10:55:43 +0200

    refactor: setup and ran prettier

--- src/providers/posts.tsx
+++ src/providers/posts.tsx
@@ -4,46 +4,44 @@ import { createServerFn } from "@tanstack/react-start";
 import axios from "redaxios";
 
 export type PostType = {
-  id: string;
-  title: string;
-  body: string;
+    id: string;
+    title: string;
+    body: string;
 };
 
-export const fetchPosts = createServerFn({ method: "GET" }).handler(
-  async () => {
+export const fetchPosts = createServerFn({ method: "GET" }).handler(async () => {
     console.info("Fetching posts...");
     return axios
-      .get<Array<PostType>>("https://jsonplaceholder.typicode.com/posts")
-      .then((r) => r.data.slice(0, 10));
-  },
-);
+        .get<Array<PostType>>("https://jsonplaceholder.typicode.com/posts")
+        .then((r) => r.data.slice(0, 10));
+});
 
 export const postsQueryOptions = () =>
-  queryOptions({
-    queryKey: ["posts"],
-    queryFn: () => fetchPosts(),
-  });
+    queryOptions({
+        queryKey: ["posts"],
+        queryFn: () => fetchPosts(),
+    });
 
 export const fetchPost = createServerFn({ method: "GET" })
-  .validator((d: string) => d)
-  .handler(async ({ data }) => {
-    console.info(`Fetching post with id ${data}...`);
-    const post = await axios
-      .get<PostType>(`https://jsonplaceholder.typicode.com/posts/${data}`)
-      .then((r) => r.data)
-      .catch((err) => {
-        console.error(err);
-        if (err.status === 404) {
-          throw notFound();
-        }
-        throw err;
-      });
+    .validator((d: string) => d)
+    .handler(async ({ data }) => {
+        console.info(`Fetching post with id ${data}...`);
+        const post = await axios
+            .get<PostType>(`https://jsonplaceholder.typicode.com/posts/${data}`)
+            .then((r) => r.data)
+            .catch((err) => {
+                console.error(err);
+                if (err.status === 404) {
+                    throw notFound();
+                }
+                throw err;
+            });
 
-    return post;
-  });
+        return post;
+    });
 
 export const postQueryOptions = (postId: string) =>
-  queryOptions({
-    queryKey: ["post", postId],
-    queryFn: () => fetchPost({ data: postId }),
-  });
+    queryOptions({
+        queryKey: ["post", postId],
+        queryFn: () => fetchPost({ data: postId }),
+    });
--- src/routes/posts.$postId.tsx
+++ src/routes/posts.$postId.tsx
@@ -1,51 +1,56 @@
-import { ErrorComponent, Link, createFileRoute } from '@tanstack/react-router'
-import { useSuspenseQuery } from '@tanstack/react-query'
-import { postQueryOptions } from '../utils/posts'
-import type { ErrorComponentProps } from '@tanstack/react-router'
-import { NotFound } from '~/components/NotFound'
+import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+    createFileRoute,
+    ErrorComponent,
+    Link,
+    type ErrorComponentProps,
+} from "@tanstack/react-router";
+import { NotFound } from "~/components/NotFound";
 
-export const Route = createFileRoute('/posts/$postId')({
-  loader: async ({ params: { postId }, context }) => {
-    const data = await context.queryClient.ensureQueryData(
-      postQueryOptions(postId),
-    )
+import { postQueryOptions } from "../providers/posts";
 
-    return {
-      title: data.title,
-    }
-  },
-  head: ({ loaderData }) => ({
-    meta: loaderData ? [{ title: loaderData.title }] : undefined,
-  }),
-  errorComponent: PostErrorComponent,
-  notFoundComponent: () => {
-    return <NotFound>Post not found</NotFound>
-  },
-  component: PostComponent,
-})
+export const Route = createFileRoute("/posts/$postId")({
+    loader: async ({ params: { postId }, context }) => {
+        const data = await context.queryClient.ensureQueryData(
+            postQueryOptions(postId)
+        );
+
+        return {
+            title: data.title,
+        };
+    },
+    head: ({ loaderData }) => ({
+        meta: loaderData ? [{ title: loaderData.title }] : undefined,
+    }),
+    errorComponent: PostErrorComponent,
+    notFoundComponent: () => {
+        return <NotFound>Post not found</NotFound>;
+    },
+    component: PostComponent,
+});
 
 export function PostErrorComponent({ error }: ErrorComponentProps) {
-  return <ErrorComponent error={error} />
+    return <ErrorComponent error={error} />;
 }
 
 function PostComponent() {
-  const { postId } = Route.useParams()
-  const postQuery = useSuspenseQuery(postQueryOptions(postId))
+    const { postId } = Route.useParams();
+    const postQuery = useSuspenseQuery(postQueryOptions(postId));
 
-  return (
-    <div className="space-y-2">
-      <h4 className="text-xl font-bold underline">{postQuery.data.title}</h4>
-      <div className="text-sm">{postQuery.data.body}</div>
-      <Link
-        to="/posts/$postId/deep"
-        params={{
-          postId: postQuery.data.id,
-        }}
-        activeProps={{ className: 'text-black font-bold' }}
-        className="block py-1 text-blue-800 hover:text-blue-600"
-      >
-        Deep View
-      </Link>
-    </div>
-  )
+    return (
+        <div className="space-y-2">
+            <h4 className="text-xl font-bold underline">{postQuery.data.title}</h4>
+            <div className="text-sm">{postQuery.data.body}</div>
+            <Link
+                to="/posts/$postId/deep"
+                params={{
+                    postId: postQuery.data.id,
+                }}
+                activeProps={{ className: "text-black font-bold" }}
+                className="block py-1 text-blue-800 hover:text-blue-600"
+            >
+                Deep View
+            </Link>
+        </div>
+    );
 }
